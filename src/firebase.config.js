66ee5a2f9eba465d557be6a6bd1602f189f4e398
Hardik Gohil <32103022+HardikGohilHLR@users.noreply.github.com>
2022-06-26 22:49:36 +0530

    env setup

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6c9AlAvgXcrKs70TdLjow8zgWEIUZTRo",
-    authDomain: "fireblogs-d6908.firebaseapp.com",
-    projectId: "fireblogs-d6908",
-    storageBucket: "fireblogs-d6908.appspot.com",
-    messagingSenderId: "150235590351",
-    appId: "1:150235590351:web:6e57182ce6b9c5cd0451ba"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
