7e81cd116b853c26f599fd0e77c0dcb928f15173
Hardik Gohil <32103022+HardikGohilHLR@users.noreply.github.com>
2022-07-10 23:44:19 +0530

    footer added

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,27 @@ const Footer = () => {
         <React.Fragment>
             <footer>
                 <div className="fb_container">
-                    <p>Copyright</p>
+                    <div className="fb_footer-text">
+                        <p>&copy; Hardik Gohil, {new Date()?.getFullYear()}</p>
+                    </div>
+
+                    <ul className="fb_footer-links">
+                        <li>
+                            <a href="https://github.com/HardikGohilHLR" target="_blank">
+                                <img src="/images/social/github.svg" alt="Github"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/hardikgohilhlr/" target="_blank">
+                                <img src="/images/social/linkedin.svg" alt="Linkedin"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/hardikgohilhlr/" target="_blank">
+                                <img src="/images/social/instagram.svg" alt="Instagram"/>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </footer>
         </React.Fragment>
